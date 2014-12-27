30da203d137b73b932fe5b066174335cd489a5bf
Troels Schoenfeldt <trsch@r2n26.esss.dk>
2014-12-27 18:06:06 +0100

    a narrow search

--- Core_Model/plotcases.c
+++ Core_Model/plotcases.c
@@ -3,16 +3,18 @@
 
 int plotcases(){
   gStyle->SetOptStat(0);
-  IMM *I=new IMM("/tmp/trsch/corebigrun/");
+  IMM *I=new IMM("narrowedsearch/");
 
   TH2D *h1=I->Get2dMap()->Clone(); // or TH2D *h1=I->Get2dMap("Keff");
-  TH2D *h2=I->Get2dMap("CaptureFrac")->Clone();
-  TH2D *h3=I->Get2dMap("EscapeFrac")->Clone();
-  TH2D *h4=I->Get2dMap("FissionFrac")->Clone();
+  //  TH2D *h2=I->Get2dMap("CaptureFrac")->Clone();
+  // TH2D *h3=I->Get2dMap("EscapeFrac")->Clone();
+  //TH2D *h4=I->Get2dMap("FissionFrac")->Clone();
 
   TCanvas *c1=new TCanvas();
   h1->Draw("colz");
-  TCanvas *c2=new TCanvas();
+
+  return;  
+TCanvas *c2=new TCanvas();
   h2->Draw("colz");
   TCanvas *c3=new TCanvas();
   h3->Draw("colz");
